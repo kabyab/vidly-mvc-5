a53b91c286ffcbb5eb82f3d1b00c912fac20bc5d
kabyab <bandokabs@gmail.com>
2017-11-25 15:21:37 +0530

    Completed Customer Model

---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public int Id { get; set; }
+        public byte SignUpFee { get; set; }
+        public short DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
